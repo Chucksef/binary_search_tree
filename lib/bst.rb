24d8523b47b60b286cf9d0c113e6f49bbb4aab13
chucksef <chucksef@gmail.com>
2019-11-27 10:04:38 -0700

    Basic classes and functions blocked out but not written

---
+++ lib/bst.rb
@@ -0,0 +1,68 @@
+class Node
+
+    def initialize
+
+    end
+
+    def compare
+        # compares the values of this node to another node
+    end
+
+end
+
+class Tree
+
+    def initialize
+        
+    end
+
+    def build_tree
+        # accepts array and builds the tree at initialization
+
+    end
+
+    def insert
+        # inserts a new node into the tree
+
+    end
+
+    def delete
+        # deletes a node from the tree
+
+    end
+
+    def find
+        # searches the tree for a value and returns a node if found
+
+    end
+
+    def breadth_first
+        # traverses the tree in breadth first order, accepting a block
+
+    end
+
+    def in_order
+        # Depth first traversal in L-D-R order
+
+    end
+
+    def pre_order
+        # Depth first traversal in D-L-R order
+
+    end
+
+    def post_order
+        # Depth first traversal in L-R-D order
+
+    end
+
+    def balanced?
+        # returns true if the tree is balanced
+
+    end
+
+    def rebalance!
+        # obviously rebalances the tree
+
+    end
+end
